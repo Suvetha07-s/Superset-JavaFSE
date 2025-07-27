1533858bb874500a16ecb161ce6361df734258a8
Suvetha07-s <suvethacsenec@gmail.com>
2025-07-27 21:44:37 +0530

    Add files via upload

---
+++ Week-6/1. ReactJS-HOL/My_first_React/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{
+      display: 'flex',
+      height: '100vh',
+      justifyContent: 'center',
+      alignItems: 'center'
+    }}>
+      <h1>Welcome to the first session of React</h1>
+    </div>
+  );
+}
+
+export default App;
